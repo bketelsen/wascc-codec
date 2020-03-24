639e8e2681b427fee6a064670781917c05e9fde2
Brian Ketelsen <mail@bjk.fyi>
2020-03-24 12:19:27 -0400

    add log level descriptions to the documentation

--- src/logging.rs
+++ src/logging.rs
@@ -9,7 +9,9 @@ pub const OP_LOG: &str = "WriteLog";
 pub struct WriteLogRequest {
     /// A string which represents the actor source of the log message
     pub actor: String,
-    /// level corresponds to the log crate's Level enum where Error = 1 and Trace = 5
+    /// level corresponds to the log level 
+    ///
+    /// "OFF"=0 , "ERROR"=1, "WARN"=2, "INFO"=3, "DEBUG"=4, "TRACE"=5
     pub level: usize,
     /// A string that represents the body of the log message
     pub body: String,
