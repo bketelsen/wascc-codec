9b0f3b5a51ba9cea570da96a8964d0fdd14efdf8
Brian Ketelsen <mail@bjk.fyi>
2020-03-24 12:15:32 -0400

    update operation to WriteLog, add appropriate doc comments

--- src/logging.rs
+++ src/logging.rs
@@ -1,15 +1,14 @@
 //! # Logging Data Types
 //!
 //! This module contains data types for the `wascc:logging` capability provider
-//! The message set is intentionally a mirror of the log crate's types.
 
-pub const OP_LOG: &str = "Log";
+pub const OP_LOG: &str = "WriteLog";
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-/// A representation of a log message
-pub struct LogRequest {
-    /// A string which represents the module or source of the log message
-    pub target: String,
+/// A representation of a request to write a log entry
+pub struct WriteLogRequest {
+    /// A string which represents the actor source of the log message
+    pub actor: String,
     /// level corresponds to the log crate's Level enum where Error = 1 and Trace = 5
     pub level: usize,
     /// A string that represents the body of the log message
