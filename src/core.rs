7ac93e56696cfbb44abb0a19229187bd525bc117
Kevin Hoffman <alothien@gmail.com>
2020-07-01 08:44:52 -0400

    Adding a few constants to support putting actor claims in a configuration hash map

--- src/core.rs
+++ src/core.rs
@@ -12,6 +12,14 @@ pub const OP_INITIALIZE: &str = "Initialize";
 pub const OP_BIND_ACTOR: &str = "BindActor";
 pub const OP_REMOVE_ACTOR: &str = "RemoveActor";
 
+// Keys used for providing actor claim data to a capability provider during binding
+
+pub const CONFIG_WASCC_CLAIMS_ISSUER: &str = "__wascc_issuer";
+pub const CONFIG_WASCC_CLAIMS_CAPABILITIES: &str = "__wascc_capabilities";
+pub const CONFIG_WASCC_CLAIMS_NAME: &str = "__wascc_name";
+pub const CONFIG_WASCC_CLAIMS_EXPIRES: &str = "__wascc_expires";
+pub const CONFIG_WASCC_CLAIMS_TAGS: &str = "__wascc_tags";
+
 /// LiveUpdate is used when a module is being replaced. The bytes contained in this message will, if valid,
 /// replace the existing actor. This message is sent to an actor from the "system" origin
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
